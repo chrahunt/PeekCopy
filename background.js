ec051e5cda23b6363cca45623263d74d8291b916
Christopher Hunt <chrahunt@gmail.com>
2013-10-15 21:57:08 -0400

    Clicking page action opens page

--- background.js
+++ background.js
@@ -1,15 +1,28 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
-  // If the letter 'g' is found in the tab's URL...
-  if (tab.url.indexOf('g') > -1) {
-    // ... show the page action.
+  if (tab.url.indexOf('duolingo.com') > -1) {
       chrome.pageAction.show(tabId);
   }
-};
-  
+}
+
 // Listen for any changes to the URL of any tab.
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+// Open page when page action is clicked
+chrome.pageAction.onClicked.addListener(function(disregard) {
+  var pageUrl = chrome.extension.getURL('test.html');
+  console.log(pageUrl);
+  chrome.tabs.query({ "url": pageUrl }, function(results) {
+    var found = false;
+    if (results.length > 0) {
+      found = true;
+      for (var i = 1; i < results.length; i++) {
+        chrome.tabs.remove(results[i].id);
+      }
+      chrome.tabs.update(results[0].id, { "selected": true });
+    }
+    if (!found) {
+      chrome.tabs.create({ "url": pageUrl });
+    }
+  });
+});
